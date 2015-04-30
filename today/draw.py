e36af6efd840a5852d02f7ac3c54b0c90b85a9d9
Xiaojun Sun <jason.sj.sun@gmail.com>
2015-04-29 21:14:50 -0600

    conclude UNSAT core efforts

--- today/draw.py
+++
@@ -1,5 +0,0 @@
-from ete2 import Tree
-
-t = Tree("unsat11.nh", format = 0)
-
-t.render("mytree2.png", w=183, units="mm")
---
+++ unsat/visualization/draw.py
@@ -0,0 +1,9 @@
+# this is based on ETE2 package, visualizing a tree written in newick format (".nh" file)
+# sudo apt-get install python-qt4 python-mysqldb setuptools (may exist typo..care)
+# sudo easy_install -U ete2
+
+from ete2 import Tree
+
+t = Tree("tier2.nh", format = 0)
+
+t.render("mytree2.png", w=183, units="mm")
