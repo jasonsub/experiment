6a8bef085edf6dd5f2a2bd9d9b5d4c7ec5481d89
Xiaojun Sun <jason.sj.sun@gmail.com>
2016-02-20 08:32:09 -0700

    unsat core refine phase iv

--- unsat/Core_Refine/parser_a.pl
+++ unsat/Core_Refine/parser_a.pl
@@ -4,6 +4,8 @@ use 5.014;
 use strict;
 use warnings;
 
+use List::MoreUtils qw(uniq);
+
 # Update: Xiaojun Sun, 02/19/2016
 # Change 1: Input format change, now first line add #var as 3rd arg, then each line index and coeff are alternating
 # e.g. line "10 2 1 5 ab 4 c" means f_10 = 1*f_2 + ab*f_5 + c*f_4
@@ -11,6 +13,7 @@ use warnings;
 # involved with s-poly/reduction (not simplified), it is meant to be fed to Singular to check if any of
 # them reduced to 0. Excluding those, remaining nonzero coeffs represents the core. Note for fitting
 # the size of total poly, coeff[last] must be defined (if not used, then define as 0)
+# Change 3: Add output file recording distance and frequency 
 
 # Note: read_in file format:
 # first line: start with "s", then a single number means # of original polys, then # of total GBs include "1" 
@@ -26,6 +29,7 @@ use warnings;
 
 open TREE, ">", $ARGV[0].".nh";  # auto-assign name
 open COEF, ">", "tmp.coef";
+open DIST, ">", "tmp.dist";
 my $cnt = 0;
 
 my $i;
@@ -34,6 +38,10 @@ my %lines;
 my $origin;
 my $n;
 my $nvars;
+my %coefs;
+my %dist;
+my %freq;
+my %olines;
 
 while(<>){
 	chomp;
@@ -57,6 +65,7 @@ while(<>){
 	  }
 	}
 	$lines{$items[0]} = $tmp;
+	$olines{$items[0]} = $read_in;  # record original contents for later analysis
 	if($items[0] =~ /e/)
 	{
 	  my @keys = sort keys %lines;
@@ -76,3 +85,71 @@ for($i = $n-1; $i > $origin; $i--)
 
 print TREE $tmp;
 close TREE;
+
+# Analysis for coef, dist and freq starts here!
+$tmp = $olines{"e"};
+my @items = split /\s+/;
+my @newpoly;
+for($i = 1; $i <= $#items; $i=$i+2)
+{
+	if($items[$i] > $origin) {@newpoly = sort {$a <=> $b} $items[$i], @newpoly;} # add new poly involved to sorted array
+	@newpoly = uniq(@newpoly);
+	if(exists $coefs{$items[$i]}) {$coefs{$items[$i]} = $coefs{$items[$i]}."+".$items[$i+1];}
+	if(!exists $coefs{$items[$i]}) {$coefs{$items[$i]} = $items[$i+1];}
+	my $tmpdist = ( $i == 1 )?($#items/2 -1):( ($#items+1-$i)/2 ); # min distance = 1, spoly have same dist
+	if(exists $dist{$items[$i]}) {$dist{$items[$i]} = ($tmpdist < $dist{$items[$i]})?$tmpdist:$dist{$items[$i]}; }
+	if(!exists $dist{$items[$i]}) {$dist{$items[$i]} = $tmpdist; }
+	if(exists $freq{$items[$i]}) {$freq{$items[$i]} ++; }
+	if(!exists $freq{$items[$i]}) {$freq{$items[$i]} = 1; }
+}
+undef @items;
+
+# Now iteratively pop largest new poly from array, do analysis
+# Note we need to consider base coeff (multiply) and base distance (add) now
+while(1)
+{
+	my $polytrace = pop @newpoly;
+	if(!defined($polytrace)) {last;}
+	print "$polytrace ";
+	$tmp = $olines{$polytrace};
+	@items = split /\s+/, $tmp;
+	for($i = 1; $i <= $#items; $i=$i+2)
+	{
+		if($items[$i] > $origin && $items[$i] < $polytrace) {@newpoly = sort {$a <=> $b} $items[$i], @newpoly;} # add new poly involved to sorted array
+		@newpoly = uniq(@newpoly);
+		# local coef multiply by coef of current poly previous stored
+		if(exists $coefs{$items[$i]}) {$coefs{$items[$i]} = $coefs{$items[$i]}."+(".$coefs{$polytrace}.")*".$items[$i+1];}
+		if(!exists $coefs{$items[$i]}) {$coefs{$items[$i]} = "(".$coefs{$polytrace}.")*".$items[$i+1];}
+		# local distance added to dist of current poly previously called
+		my $tmpdist = $dist{$polytrace} + ( $i == 1 )?($#items/2 -1):( ($#items+1-$i)/2 ); # min distance = 1, spoly have same dist
+		if(exists $dist{$items[$i]}) {$dist{$items[$i]} = ($tmpdist < $dist{$items[$i]})?$tmpdist:$dist{$items[$i]}; }
+		if(!exists $dist{$items[$i]}) {$dist{$items[$i]} = $tmpdist; }
+		if(exists $freq{$items[$i]}) {$freq{$items[$i]} ++; }
+		if(!exists $freq{$items[$i]}) {$freq{$items[$i]} = 1; }
+	}
+}
+
+# Print coef files send to Singular for vanishing coef checking (if there exists)
+print COEF "ring Q = 2, (";
+for($i = 1; $i < $nvars; $i++) {
+	print COEF "v$i,";
+}
+print COEF "v$nvars), Dp;\nideal CF;\n";
+for($i = 1; $i < $origin; $i++) {
+	if(exists $coefs{$i}) {printf COEF "CF[%d] = %s;\n", $i, $coefs{$i}; }
+}
+if(exists $coefs{$origin}) {
+	printf COEF "CF[%d] = %s\n;", $origin, $coefs{$origin};
+}
+else {
+	print COEF "CF[$i] = 0;\n";
+}
+print COEF "string st = \"\";\nfor(int i = 1; i <= $origin; i++)\n\{\n\tif(CF[i] != 0) \{\n\t\tst = st+string(i)+\" \";\n\t\}\n\}\n";
+print COEF "link l = \":w tmp.simp\";\nwrite(l,st);\nclose(l);\nquit;";
+close COEF;
+
+# Print dist files for reordering, format is poly index -> dist -> freq
+print DIST "c idx dist freq\n";
+for($i = 1; $i <= $origin; $i++) {
+	if(exists $dist{$i}) {printf DIST "%d %d %d\n", $i, $dist{$i}, $freq{$i}; }
+}
