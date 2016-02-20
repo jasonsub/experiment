5caf54fb92ec521cadd08c83d343d81d88e82195
Xiaojun Sun <jason.sj.sun@gmail.com>
2016-02-19 17:53:33 -0700

    core refinement phase iii

--- unsat/Core_Refine/parser_a.pl
+++ unsat/Core_Refine/parser_a.pl
@@ -4,6 +4,14 @@ use 5.014;
 use strict;
 use warnings;
 
+# Update: Xiaojun Sun, 02/19/2016
+# Change 1: Input format change, now first line add #var as 3rd arg, then each line index and coeff are alternating
+# e.g. line "10 2 1 5 ab 4 c" means f_10 = 1*f_2 + ab*f_5 + c*f_4
+# Change 2: Besides newick tree output, added another output file listing coefficients for each poly
+# involved with s-poly/reduction (not simplified), it is meant to be fed to Singular to check if any of
+# them reduced to 0. Excluding those, remaining nonzero coeffs represents the core. Note for fitting
+# the size of total poly, coeff[last] must be defined (if not used, then define as 0)
+
 # Note: read_in file format:
 # first line: start with "s", then a single number means # of original polys, then # of total GBs include "1" 
 # followed by n lines means n new polys added to GB
@@ -16,7 +24,8 @@ use warnings;
 # Objective: translate the output from singular file (computing GB for UNSAT) showing all original polys involved for
 # spoly and reduction to newick tree format displaying the hierarchy
 
-open TREE, ">", "unsat11.nh";  # change the name
+open TREE, ">", $ARGV[0].".nh";  # auto-assign name
+open COEF, ">", "tmp.coef";
 my $cnt = 0;
 
 my $i;
@@ -24,6 +33,7 @@ my $tmp;
 my %lines;
 my $origin;
 my $n;
+my $nvars;
 
 while(<>){
 	chomp;
@@ -34,13 +44,14 @@ while(<>){
 	if($items[0] =~ /s/){  # starting line
 	  $origin = $items[1];
 	  $n = $items[2];
+	  $nvars = $items[3];
 	  next;
 	}
 	$cnt++;
-	$tmp = "(".$items[1].":1,".$items[2].":1)";
-	if(defined($items[3]))  # this line has reduction polys
+	$tmp = "(".$items[1].":1,".$items[3].":1)";
+	if(defined($items[5]))  # this line has reduction polys
 	{
-	  for($i = 3; $i <= $#items; $i++)
+	  for($i = 5; $i <= $#items; $i=$i+2)
 	  {
 		$tmp = "(".$tmp.":0.5,".$items[$i].":0.5)";
 	  }
